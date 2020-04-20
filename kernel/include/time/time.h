1a1fe6a0c1b116951360636300a3cfadba248597
VXdora <naoki5336@live.jp>
2020-04-20 18:23:11 +0900

    time module header

---
+++ kernel/include/time/time.h
@@ -0,0 +1,14 @@
+// *********************************
+//  include - device - time.h
+//  Update  develop3.5
+// *********************************
+
+#include <types.h>
+
+#ifndef __dTIME__
+#define __dTIME__       // __TIME__は使えない
+
+Time getTime(void);
+int  getTimeData(char addr);
+
+#endif  // __dTIME__
